60901d4a98254ce2f559575526cf45e467a0c619
wilhas125 <wilhas125@hotmail.com>
2023-05-09 08:17:03 -0300

    add projeto

--- Projeto vendas Fatec/br.com.projeto.view/Form1.Designer.cs
+++ Projeto vendas Fatec/br.com.projeto.view/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.txtCodigoCupom = new System.Windows.Forms.TextBox();
             this.txtFormaPagamento = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtNomeEstabelecimento
@@ -149,11 +150,21 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(629, 164);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Cupom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
@@ -189,5 +200,6 @@
         private System.Windows.Forms.TextBox txtCodigoCupom;
         private System.Windows.Forms.TextBox txtFormaPagamento;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }

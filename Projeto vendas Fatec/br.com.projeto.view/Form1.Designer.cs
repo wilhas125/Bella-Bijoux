5aa425994599bcb003e36d7a1a0db60e2a40a5cc
wilhas125 <wilhas125@hotmail.com>
2023-05-09 08:21:15 -0300

    add projeto

--- Projeto vendas Fatec/br.com.projeto.view/Form1.Designer.cs
+++ Projeto vendas Fatec/br.com.projeto.view/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.txtFormaPagamento = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtNomeEstabelecimento
@@ -159,11 +160,21 @@
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(363, 214);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Cupom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
@@ -201,5 +212,6 @@
         private System.Windows.Forms.TextBox txtFormaPagamento;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
